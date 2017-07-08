60e0c54ad1f0586943322a3599f15a7a6497fe89
“Alice <aliceletourneur@icloud.com>
2017-07-08 16:02:35 +0100

    Rewriting from scratch app.js Adding a model, controller and view objects. Initiate the app

--- js/app.js
+++ js/app.js
@@ -1,84 +1,94 @@
-/* STUDENTS IGNORE THIS FUNCTION
- * All this does is create an initial
- * attendance record if one is not found
- * within localStorage.
- */
-(function() {
-    if (!localStorage.attendance) {
-        console.log('Creating attendance records...');
-        function getRandom() {
-            return (Math.random() >= 0.5);
-        }
-
-        var nameColumns = $('tbody .name-col'),
-            attendance = {};
-
-        nameColumns.each(function() {
-            var name = this.innerText;
-            attendance[name] = [];
-
-            for (var i = 0; i <= 11; i++) {
-                attendance[name].push(getRandom());
-            }
-        });
-
-        localStorage.attendance = JSON.stringify(attendance);
+/* ============== Model ============== */
+var model = {
+  // Array of students
+  student:[
+    {
+      name: "Slappy the Frog",
+      attendance : [],
+      daysMissed : 0
+    },
+    {
+      name: "Lilly the Lizard",
+      attendance : [],
+      daysMissed : 0
+    },
+    {
+      name: "Paulrus the Walrus",
+      attendance : [],
+      daysMissed : 0
+    },
+    {
+      name: "Gregory the Goat",
+      attendance : [],
+      daysMissed : 0
+    },
+    {
+      name: "Adam the Anaconda",
+      attendance : [],
+      daysMissed : 0
     }
-}());
+  ]
+};
+/* ============== Controller ============== */
+var controller = {
+  // Initialise controller setting all attendance for each students to false
+  init: function() {
+    for (var i = 0; i < model.student.length ; i++){
+      this.attendance = model.student[i].attendance;
+      for (var j = 0 ; j < 12 ; j++){
+        this.attendance.push(false);
+      }
+    };
+    view.init();
+  },
 
+  // Get all students data
+  getStudents: function() {
+    return model.student;
+  }
+};
 
-/* STUDENT APPLICATION */
-$(function() {
-    var attendance = JSON.parse(localStorage.attendance),
-        $allMissed = $('tbody .missed-col'),
-        $allCheckboxes = $('tbody input');
+/* ============== View ============== */
 
-    // Count a student's missed days
-    function countMissing() {
-        $allMissed.each(function() {
-            var studentRow = $(this).parent('tr'),
-                dayChecks = $(studentRow).children('td').children('input'),
-                numMissed = 0;
-
-            dayChecks.each(function() {
-                if (!$(this).prop('checked')) {
-                    numMissed++;
-                }
-            });
-
-            $(this).text(numMissed);
-        });
+var view = {
+  // Render the initial view of the list
+  init: function() {
+    this.studentList = document.getElementById('studentList');
+    var students = controller.getStudents();
+    this.studentList.innerHTML = '';
+    for (var i = 0 ; i < students.length; i++) {
+      var student = students[i];
+      // Create a new row
+      var studentElem = document.createElement('tr');
+      studentElem.setAttribute("class", "student");
+      this.studentList.appendChild(studentElem);
+      // Add the name of the student in first column
+      var studentNameElem = document.createElement('td');
+      studentNameElem.setAttribute("class", "name-col");
+      studentNameElem.textContent = student.name;
+      studentElem.appendChild(studentNameElem);
+      // Add a checkbox in the next 12 columns
+      for (var e = 0 ; e < 12 ; e++){
+        var attendance = student.attendance[e];
+        var studentAttendanceElem = document.createElement('td');
+        studentAttendanceElem.setAttribute("class", "attend-col");
+        studentElem.appendChild(studentAttendanceElem);
+        var studentAttendanceInputElem = document.createElement('input');
+        studentAttendanceInputElem.setAttribute("type", "checkbox");
+        // Checkbox will be ticked if attendance is set to true for the day
+        if (attendance == true){
+          studentAttendanceInputElem.setAttribute("checked", "");
+        };
+        studentAttendanceElem.appendChild(studentAttendanceInputElem);
+      }
+      // Add the total number of days missed by the student in the last column
+      var studentMissedDaysElem = document.createElement('td');
+      studentMissedDaysElem.setAttribute("class", "missed-col");
+      studentMissedDaysElem.textContent = student.daysMissed;
+      studentElem.appendChild(studentMissedDaysElem);
     }
+  }
+};
 
-    // Check boxes, based on attendace records
-    $.each(attendance, function(name, days) {
-        var studentRow = $('tbody .name-col:contains("' + name + '")').parent('tr'),
-            dayChecks = $(studentRow).children('.attend-col').children('input');
-
-        dayChecks.each(function(i) {
-            $(this).prop('checked', days[i]);
-        });
-    });
-
-    // When a checkbox is clicked, update localStorage
-    $allCheckboxes.on('click', function() {
-        var studentRows = $('tbody .student'),
-            newAttendance = {};
-
-        studentRows.each(function() {
-            var name = $(this).children('.name-col').text(),
-                $allCheckboxes = $(this).children('td').children('input');
-
-            newAttendance[name] = [];
-
-            $allCheckboxes.each(function() {
-                newAttendance[name].push($(this).prop('checked'));
-            });
-        });
-
-        countMissing();
-        localStorage.attendance = JSON.stringify(newAttendance);
-    });
-
-    countMissing();
-}());
+/* ========= Initiate the app =========== */
+controller.init();
